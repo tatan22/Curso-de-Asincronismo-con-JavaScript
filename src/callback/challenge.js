d7a953ad0acccf14d1a0a8afb70a9cbd2f2fa727
Jhonatan-Cardona <tatancarduar@gmail.com>
2023-02-11 12:36:49 -0500

    Segundo commit perla

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,5 +1,5 @@
-const XMLHttpRequest = require('xmlhttprequest');
-const API = 'https://api.escuelajs.co/api/v1';
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const API = "https://api.escuelajs.co/api/v1";
 
 function fetchData(urlAPI, callback) {
     let xhttp = new XMLHttpRequest();
@@ -8,14 +8,25 @@ function fetchData(urlAPI, callback) {
     xhttp.onreadystatechange = function (event){
         if(xhttp.readyState === 4){
             if(xhttp.status === 200) {
-                callback(null, JSON.parse(resposeTexr));
-            }
-        
-        } else{
-            const error = new Error('Error' + urlAPI);
-            return callback(error, null);
-        }
-         
+                callback(null, JSON.parse(xhttp.responseText));
+            }else{
+                const error = new Error('Error' + urlAPI);
+                return callback(error, null);
+            }       
+        }          
     }
     xhttp.send();
 }
+
+fetchData(`${API}/products`, function (error1, data1){
+    if(error1) return console.error(error1);
+    fetchData(`${API}/products/${data1[0].id}`, function (error2, data2){
+        if(error2)return console.error(error2);
+        fetchData(`${API}/categories/${data2?.category?.id}`, function (error3, data3){
+            if(error3)return console.error(error3);
+            console.log(data1[0]);
+            console.log(data2.title);
+            console.log(data3.name);
+        }) ;
+    });
+});
